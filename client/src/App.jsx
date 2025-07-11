2d719f0e432cb5f35b59a4683dfbac918843fae4
Rashmi Choudhary <161102161+Code-123budd@users.noreply.github.com>
2025-07-11 18:44:23 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import {BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Auth from './pages/auth/Auth'
 import Dashboard from './pages/dashboard/Dashboard.jsx'
-import IsLogin from './pages/auth/isLogin.jsx'
+import IsLogin from './pages/auth/IsLogin.jsx'
 
 function App() {
   
