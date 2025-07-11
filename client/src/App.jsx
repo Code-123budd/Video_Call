b15d45d9232c4e9503a13bdd7230b64c2adf0400
Rashmi Choudhary <161102161+Code-123budd@users.noreply.github.com>
2025-07-11 18:41:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './index.css'
 import {BrowserRouter as Router,Routes,Route } from 'react-router-dom'
 import Auth from './pages/auth/Auth'
-import Dashboard from './pages/dashboard/dashboard'
+import Dashboard from './pages/dashboard/Dashboard.jsx'
 import IsLogin from './pages/auth/isLogin.jsx'
 
 function App() {
