780737c03b80dd471a059beef396a0e8a491ccc3
Rashmi Choudhary <161102161+Code-123budd@users.noreply.github.com>
2025-07-13 23:18:12 +0530

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,6 +1,8 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+// ✅ CORRECT way as per Tailwind's latest postcss plugin
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer')
+  ],
+}
+
