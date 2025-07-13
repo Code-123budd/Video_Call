9fbe03dfb2b824c51c1bb878d99bfb970267b4be
Rashmi Choudhary <161102161+Code-123budd@users.noreply.github.com>
2025-07-13 23:46:14 +0530

    Update postcss.config.js

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,9 +1,4 @@
 // postcss.config.js (ESM format)
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss(), autoprefixer()],
-};
+// this file is deleted
 
 
