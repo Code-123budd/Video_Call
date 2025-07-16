0c12975111e384a6098349b5a4b7cb997c97f189
Rashmi Choudhary <161102161+Code-123budd@users.noreply.github.com>
2025-07-16 15:38:28 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,4 +1,4 @@
-import tailwindcss from '@tailwindcss/vite'
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
